e2ca9d2f35d9ceae9fde97cd7cb958697cd81a28
Newton Vu <newtonvu1@gmail.com>
2024-02-23 00:04:47 -0500

    Fixed merging issues

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Beginning of the login.html file
+var Login = document.getElementById('login');
+var Register = document.getElementById('register');
+var LoginBubble = document.getElementById('button-color');
+
+function register() {
+    Login.style.left = "-400px";
+    Register.style.left = "0px";
+    LoginBubble.style.left = "150px";
+}
+function login() {
+    Login.style.left = "0px";
+    Register.style.left = "400px";
+    LoginBubble.style.left = "0px";
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const taskInput = document.getElementById('task-box');
     const addButton = document.querySelector('button[title="Add task"]');
